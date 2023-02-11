320496832e073d82955386de03457e67cc0e50aa
Vincenzo Petrolo <s295043@studenti.polito.it>
2023-02-11 01:23:42 -0600

    fix : print_result typo fixed

---
+++ riscv_asm/aimed_tests.c
@@ -0,0 +1,235 @@
+void do_montecarlo_alu_test() {
+    int* operands1, *operands2;
+
+    operands1[0] = 173843807;
+    operands1[1] = 1704062081;
+    operands1[2] = 837871530;
+    operands1[3] = 1117861568;
+    operands1[4] = 588267216;
+    operands1[5] = 1855602236;
+    operands1[6] = 1017943043;
+    operands1[7] = 711779280;
+    operands1[8] = 161330468;
+    operands1[9] = 665198462;
+    operands1[10] = 1461755998;
+    operands1[11] = 1355898475;
+    operands1[12] = 847222918;
+    operands1[13] = 1113733855;
+    operands1[14] = 1921209940;
+    operands1[15] = 1883324578;
+    operands1[16] = 60137093;
+    operands1[17] = 364868029;
+    operands1[18] = 2128962560;
+    operands1[19] = 1078989067;
+    operands1[20] = 1165501632;
+    operands1[21] = 889633459;
+    operands1[22] = 1041571945;
+    operands1[23] = 271734578;
+    operands1[24] = 1575653573;
+    operands1[25] = 475965100;
+    operands1[26] = 1610799022;
+    operands1[27] = 727002931;
+    operands1[28] = 1461234417;
+    operands1[29] = 1263696858;
+    operands1[30] = 1133647719;
+    operands1[31] = 190694370;
+    operands1[32] = 154795067;
+    operands1[33] = 2058996146;
+    operands1[34] = 493292283;
+    operands1[35] = 381289658;
+    operands1[36] = 1370804436;
+    operands1[37] = 1760414878;
+    operands1[38] = 1782095975;
+    operands1[39] = 382693769;
+    operands1[40] = 1700711706;
+    operands1[41] = 1390848972;
+    operands1[42] = 405604673;
+    operands1[43] = 184485015;
+    operands1[44] = 4559490;
+    operands1[45] = 35047513;
+    operands1[46] = 717530090;
+    operands1[47] = 868216992;
+    operands1[48] = 743589772;
+    operands1[49] = 1017253590;
+    operands1[50] = 1964129452;
+    operands1[51] = 760699956;
+    operands1[52] = 358165774;
+    operands1[53] = 1408047937;
+    operands1[54] = 465245143;
+    operands1[55] = 1524735470;
+    operands1[56] = 395066054;
+    operands1[57] = 1184711227;
+    operands1[58] = 1156014845;
+    operands1[59] = 1581560203;
+    operands1[60] = 2031370142;
+    operands1[61] = 2118255537;
+    operands1[62] = 1175455736;
+    operands1[63] = 1136347847;
+    operands1[64] = 2010704543;
+    operands1[65] = 1687576893;
+    operands1[66] = 1040440890;
+    operands1[67] = 1625710784;
+    operands1[68] = 240275483;
+    operands1[69] = 1297382724;
+    operands1[70] = 2070550727;
+    operands1[71] = 848843292;
+    operands1[72] = 335388748;
+    operands1[73] = 397678604;
+    operands1[74] = 273215860;
+    operands1[75] = 357726160;
+    operands1[76] = 2000686096;
+    operands1[77] = 1206266571;
+    operands1[78] = 1732514927;
+    operands1[79] = 715974760;
+    operands1[80] = 726272594;
+    operands1[81] = 335493081;
+    operands1[82] = 762893168;
+    operands1[83] = 1549133276;
+    operands1[84] = 77521464;
+    operands1[85] = 692330949;
+    operands1[86] = 1627376589;
+    operands1[87] = 480103357;
+    operands1[88] = 1050213781;
+    operands1[89] = 2078470949;
+    operands1[90] = 1975955514;
+    operands1[91] = 1332600754;
+    operands1[92] = 263521350;
+    operands1[93] = 1943997841;
+    operands1[94] = 997630488;
+    operands1[95] = 1900665728;
+    operands1[96] = 1308534834;
+    operands1[97] = 317433661;
+    operands1[98] = 641615527;
+    operands1[99] = 165357937;
+    operands2[0] = 296858980;
+    operands2[1] = 1553832757;
+    operands2[2] = 847406097;
+    operands2[3] = 340237736;
+    operands2[4] = 1049895598;
+    operands2[5] = 501267160;
+    operands2[6] = 1628948312;
+    operands2[7] = 580552567;
+    operands2[8] = 1501212595;
+    operands2[9] = 125592461;
+    operands2[10] = 873071764;
+    operands2[11] = 1986434529;
+    operands2[12] = 1670603695;
+    operands2[13] = 334941479;
+    operands2[14] = 2064157260;
+    operands2[15] = 493034182;
+    operands2[16] = 1118328704;
+    operands2[17] = 797860261;
+    operands2[18] = 1276495038;
+    operands2[19] = 2032634548;
+    operands2[20] = 914012235;
+    operands2[21] = 2124102043;
+    operands2[22] = 1227891392;
+    operands2[23] = 785451520;
+    operands2[24] = 798492351;
+    operands2[25] = 1528812055;
+    operands2[26] = 699864826;
+    operands2[27] = 1451375276;
+    operands2[28] = 1017397859;
+    operands2[29] = 1377672238;
+    operands2[30] = 1431568803;
+    operands2[31] = 1128270061;
+    operands2[32] = 1517846238;
+    operands2[33] = 665034027;
+    operands2[34] = 321953402;
+    operands2[35] = 664096334;
+    operands2[36] = 806718414;
+    operands2[37] = 978501128;
+    operands2[38] = 1683591598;
+    operands2[39] = 1534427939;
+    operands2[40] = 904752230;
+    operands2[41] = 336428374;
+    operands2[42] = 1548369467;
+    operands2[43] = 1400729259;
+    operands2[44] = 553209671;
+    operands2[45] = 714228984;
+    operands2[46] = 219927454;
+    operands2[47] = 546574447;
+    operands2[48] = 452817859;
+    operands2[49] = 294662955;
+    operands2[50] = 1231271016;
+    operands2[51] = 490644473;
+    operands2[52] = 490586089;
+    operands2[53] = 303128216;
+    operands2[54] = 1784335984;
+    operands2[55] = 947202757;
+    operands2[56] = 671088056;
+    operands2[57] = 1664692093;
+    operands2[58] = 555490890;
+    operands2[59] = 1440236301;
+    operands2[60] = 11647779;
+    operands2[61] = 1584953022;
+    operands2[62] = 1538542520;
+    operands2[63] = 1783072343;
+    operands2[64] = 652911925;
+    operands2[65] = 945725121;
+    operands2[66] = 1532259226;
+    operands2[67] = 1710278861;
+    operands2[68] = 688030256;
+    operands2[69] = 1636612548;
+    operands2[70] = 465833630;
+    operands2[71] = 991740849;
+    operands2[72] = 1688013388;
+    operands2[73] = 803991603;
+    operands2[74] = 1684853412;
+    operands2[75] = 1654521429;
+    operands2[76] = 64477405;
+    operands2[77] = 1024382262;
+    operands2[78] = 2116815929;
+    operands2[79] = 1644960962;
+    operands2[80] = 1488890409;
+    operands2[81] = 496580658;
+    operands2[82] = 1626151004;
+    operands2[83] = 56929672;
+    operands2[84] = 434270664;
+    operands2[85] = 1561201936;
+    operands2[86] = 191017971;
+    operands2[87] = 795586326;
+    operands2[88] = 1698458406;
+    operands2[89] = 1852704294;
+    operands2[90] = 1132140642;
+    operands2[91] = 1756441776;
+    operands2[92] = 328958466;
+    operands2[93] = 838966178;
+    operands2[94] = 79007656;
+    operands2[95] = 116427415;
+    operands2[96] = 1704721228;
+    operands2[97] = 757212264;
+    operands2[98] = 975229091;
+    operands2[99] = 31996586;
+
+    volatile int result;
+
+    for (int i = 0; i < 100; i++)
+    {
+        // ADDITION
+        result = operands1[i] + operands2[i];
+        // SUBTRACTION 
+        result = operands1[i] - operands2[i];
+
+        // Comparison lower-equal
+        result = (operands1[i] <= operands2[i])? 1 : 0;
+
+        // Comparison greater-equal
+        result = (operands1[i] >= operands2[i])? 1 : 0;
+
+        // AND
+        result = operands1[i] & operands2[i];
+        // OR
+        result = operands1[i] | operands2[i];
+        // XOR
+        result = operands1[i] ^ operands2[i];
+
+        // SLL
+        result = operands1[i] << operands2[i];
+
+        // SRL
+        result = operands1[i] >> operands2[i];
+
+    }
+    
+}
